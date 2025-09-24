232fc0a15b358b7b93212d360210e1176fa50f1e
Caleb <caleb.mj.chilson@gmail.com>
2025-09-24 09:39:08 +0200

    Created basic empty functions.
    I set up the basic functions we are going to use and screen modes. Also added FPS counter.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,91 @@
-int main()
+#include <raylib.h>
+#include <iostream>
+
+using namespace std;
+
+void RenderBackground()
 {
 	
+}
+
+void RenderPlayer()
+{
+	
+}
+
+void RenderEnemy()
+{
+	
+}
+
+void CheckCollision()
+{
+	
+}
+
+//Screen Modes
+
+void TitleScreen()
+{
+	
+}
+
+void GameScreen()
+{
+	RenderBackground();
+	RenderPlayer();
+	RenderEnemy();
+	CheckCollision();
+}
+
+void DeathScreen()
+{
+	
+}
+
+
+int main()
+{
+	//Variables
+	int ScreenMode = 1;
+
+	float windowWidth = 1280;
+	float windowHeight = 1024;
+
+	InitWindow(windowWidth, windowHeight, "Untitled Shooter Game");
+
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+	{
+		//Initialize
+		BeginDrawing();
+
+		ClearBackground(BLACK);
+
+		DrawFPS(100, 100);
+
+		//Gameplay
+		switch (ScreenMode)
+		{
+		case 0:
+			TitleScreen();
+			break;
+		case 1:
+			GameScreen();
+			break;
+		case 2:
+			DeathScreen();
+			break;
+		default:
+			cout << "Error, went past case 3 in Screens";
+			break;
+		}
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
 	return 0;
 }
